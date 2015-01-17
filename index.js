33110a730637b52ae3cfbe91ba2c58699d4a4a97
Dan VerWeire <dverweire@gmail.com>
2015-01-17 00:31:23 -0500

    progress

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ var redis = require('redis')
 	, jsonify = require('redis-jsonify')
 	, myip = require('my-ip')
 	, semver = require('semver')
+	, EventEmitter = require('events').EventEmitter
+	, inherits = require('util').inherits
 	;
 
 module.exports = SeaRedis;
 
 SeaRedis.connect = function (host, port) {
-	return new SeaRedis({
-		host : host
-		, port : port
-	});
-}
+	EventEmitter.call(this);
 
+	var opts;
+
+	if (typeof host === 'object') {
+		opts = host;
+	}
+	else {
+		opts = {
+			host : host
+			, port : port
+		};
+	}
+
+	return new SeaRedis(opts);
+};
+
+inherits(SeaRedis, EventEmitter);
 
 function SeaRedis (opts) {
 	var self = this;
@@ -23,11 +37,40 @@ function SeaRedis (opts) {
 	self.registrationTimeout = opts.registrationTimeout || 10000;
 	self.prefix = opts.prefix || 'SeaRedis';
 	self.redis = redis.createClient(opts.port, opts.host);
+	self.redisNotifications = redis.createClient(opts.port, opts.host);
 	self.initialized = false;
 	self.services = {};
+	self.servicesDispersed = {};
 
 	jsonify(self.redis);
+
+	self.redisNotifications.psubscribe('__keyspace@*__:"' + self.prefix + ':*');
+	self.redisNotifications.on('pmessage', function (pattern, key, event) {
+		if (event === 'expired' || event === 'del') {
+			key = /\"([^\"]*)"/.exec(key)[1];
+
+			var parsedKey = parseKey(key);
+
+			self.emit('free', self.servicesDispersed[parsedKey.id] || parsedKey);
+				
+			if (self.servicesDispersed[parsedKey.id]) {
+				self.servicesDispersed[parsedKey.id].emit('free');
+
+				//since this service is now gone, delete it
+				delete self.servicesDispersed[parsedKey.id];
+			}
+		}
+	});
 }
+
+SeaRedis.prototype.close = function () {
+	var self = this;
+
+	self.free(function () {
+		self.redis.unref();
+		self.redisNotifications.unref();
+	});
+};
 
 SeaRedis.prototype.nextIndex = function (cb) {
 	var self = this;
@@ -71,7 +114,7 @@ SeaRedis.prototype.register = function (role, opts, cb) {
 
 		register(service, function (err, result) {
 			if (result === 'OK') {
-				self.services[role].push(service);
+				self.services[role].push(new SeaRedisService(service));
 			}
 
 			service.interval = setInterval(function () {
@@ -91,15 +134,20 @@ SeaRedis.prototype.register = function (role, opts, cb) {
 	}
 };
 
-SeaRedis.prototype.get = function (role, cb) {
-	var self = this
-		, parsedRole = parseRole(role)
-		, search = self.key(parsedRole.role, '*')
+SeaRedis.prototype.query = function (role, cb) {
+	var self = this;
+
+	if (typeof role === 'function') {
+		cb = role;
+		role = null;
+	}
+
+	var  parsedRole = parseRole(role)
+		, search = self.key(parsedRole.role || '*', '*')
 		;
 
 	self.redis.keys(search, function (err, result) {
 		if (!result.length) {
-			//TODO: wait and try again until one becomes available.
 			return cb(null, []);
 		}
 
@@ -112,14 +160,19 @@ SeaRedis.prototype.get = function (role, cb) {
 
 			var parsedKey = parseKey(role);
 
-			if (!parsedKey.version && !parsedRole.version || semver.satisfies(parsedKey.version, '^' + parsedRole.version)) {
-				matches.push(parsedKey);
+			if (!parsedRole.role || !parsedKey.version && !parsedRole.version || semver.satisfies(parsedKey.version, parsedRole.version)) {
+				//this allows us to keep track of the services that we've handed
+				//out so that we can later emit events on those services that
+				//have been handed out; aka: dispersed
+				if (!self.servicesDispersed[parsedKey.id]) {
+					self.servicesDispersed[parsedKey.id] = new SeaRedisService(parsedKey);
+				}
+
+				matches.push(self.servicesDispersed[parsedKey.id]);
 			}
 		});
 
 		if (!matches.length) {
-			//TODO: just wait longer
-
 			return cb(null, []);
 		}
 
@@ -127,22 +180,59 @@ SeaRedis.prototype.get = function (role, cb) {
 	});
 };
 
-SeaRedis.prototype.free = function (role, cb) {
+SeaRedis.prototype.get = function (role, cb) {
 	var self = this;
 
-	if (!role) {
-		//remove all of the services that this instance has registered
-		Object.keys(self.services).forEach(function (role) {
-			self.services[role].forEach(function (service) {
-				clearInterval(service.interval);
+	self.query(role, function (err, services) {
+		if (err || services.length) {
+			return cb(err, services);
+		}
 
+		//nothing was returned, try again in a few
+		setTimeout(self.get.bind(self, role, cb), 500);
+	});
+};
+
+SeaRedis.prototype.free = function (targetRole, cb) {
+	var self = this
+		, pending = 0
+		;
+	if (typeof targetRole == 'function') {
+		cb = targetRole;
+		targetRole = null;
+	}
+
+	if (!Object.keys(self.services).length) {
+		return cb();
+	}
+
+	//go through all of the registered services and roles
+	Object.keys(self.services).forEach(function (role) {
+		//don't process further if we don't want to free this role
+		if (targetRole && targetRole !== role) {
+			return;
+		}
+
+		//otherwise free each service for this role
+		self.services[role].forEach(function (service) {
+			clearInterval(service.interval);
+			
+			pending += 1;
+
+			self.redis.del(service.key, function () {
 				//TODO: check for errors?
-				self.redis.del(service.key);
+
+				pending -= 1;
+				
+				if (pending === 0) {
+					delete self.services[role];
+					return cb && cb();
+				}
 			});
 		});
+	});
 
-		self.service = {};
-	}
+	return self;
 };
 
 SeaRedis.prototype.key = function () {
@@ -155,6 +245,8 @@ SeaRedis.prototype.key = function () {
 };
 
 function parseRole (role) {
+	role = role || "";
+
 	return {
 		role : role.split('@')[0]
 		, version : role.split('@')[1]
@@ -168,20 +260,22 @@ function parseKey (key) {
 		, id : key.split(':')[3]
 		, address : key.split(':')[4]
 		, port : key.split(':')[5]
+		, key : key
 	};
 }
 
-a = new SeaRedis();
+function SeaRedisService (init) {
+	EventEmitter.call(this);
+	
+	var self = this;
 
-a.register('role', function () {
-	a.get('role', function (err, services) {
-		console.log(arguments);
-	});
+	self.id = init.id;
+	self.role = init.role;
+	self.version = init.version;
+	self.address = init.address;
+	self.port = init.port;
+	self.key = init.key;
+}
 
-	setTimeout(function () {
-		console.log('calling free');
-		a.free();
-	}, 1000);
-});
-
+inherits(SeaRedisService, EventEmitter);
 
